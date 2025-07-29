02a0cb994c8ebfad60b57c0ec13bc466bb98a384
Arjun Agnihotri <arjunagnihotrikanpur@gmail.com>
2025-07-30 02:22:49 +0530

    Created Central Error Handling System

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,39 @@
+const errorMiddleware = (err, res, res, next) => {
+  try {
+    let error = { ...err };
+
+    error.message = err.message;
+    console.error(err);
+
+    // Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      const message = "Resource Not found";
+
+      error = new Error(message);
+      error.statusCode = 404;
+    }
+
+    // Mongoose Duplicate Key
+    if (err.code === 11000) {
+      const message = "Duplicate Field Value Entererd";
+
+      error = new Error(message);
+      error.statusCode = 400;
+    }
+
+    // Mongoose Validation Error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((val) => val.message);
+      error = new Error(message.join(", "));
+      error.statusCode = 400;
+    }
+
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message || "Server Error" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
