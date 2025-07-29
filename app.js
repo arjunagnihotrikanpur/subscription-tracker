ceb773b5b1751041a2e3bcd0099d1f687d24e389
Arjun Agnihotri <arjunagnihotrikanpur@gmail.com>
2025-07-30 01:34:26 +0530

    Added Mongoose Connection Logic

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 
+import connectToDatabase from "./database/mongodb.js";
+
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
@@ -17,10 +19,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(
-    `Subscription Tracker API is Running on http://localhost:${PORT}`
+    `Subscription Tracker API is Running on: http://localhost:${PORT}`
   );
+
+  await connectToDatabase();
 });
 
 export default app;
